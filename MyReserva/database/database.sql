1e359ac08009729f7c01e081f6bf9c110bea2235
jadriancz <jadriancz@gmail.com>
2019-09-26 02:33:18 -0500

    se sube Reserva

--- MyReserva/database/database.sql
+++ MyReserva/database/database.sql
@@ -1,15 +1,10 @@
-CREATE DATABASE ng_games;
+CREATE DATABASE restaurante;
 
-USE ng_games;
-
-CREATE TABLE game (
+CREATE TABLE restaurante.restaurantes (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(180),
-    description VARCHAR(255),
-    image VARCHAR(200),
+    nombre VARCHAR(180),
+    direccion VARCHAR(255),
+    telefono VARCHAR(200),
+	idExterno VARCHAR(200),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-RENAME TABLE game to games;
-
-DESCRIBE game;
